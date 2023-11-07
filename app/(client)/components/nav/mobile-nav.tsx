5ee88395c4b38507d252f53cb7fa2da5434ebd96
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-11-06 21:09:56 -0700

    mobile nav

--- app/(client)/components/nav/mobile-nav.tsx
+++ app/(client)/components/nav/mobile-nav.tsx
@@ -9,69 +9,62 @@ import {marketingConfig} from "@/config/marketing";
 import {siteConfig} from "@/config/site";
 import {useCart} from "@/context/cart";
 import {Button} from "@/app/(client)/components/ui/button";
-const sidebar = {
-  open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 100% 0)`,
-    transition: {
-      type: "spring",
-      stiffness: 20,
-      restDelta: 2,
-    },
-  }),
-  closed: {
-    clipPath: "circle(0px at 100% 0)",
-    transition: {
-      type: "spring",
-      stiffness: 400,
-      damping: 40,
-    },
-  },
+import {LinkButton} from "@/app/(client)/components/ui/link";
+import {cn} from "@/lib/utils";
+import {ro} from "date-fns/locale";
+import {set} from "date-fns";
+import {useRouter} from "next/navigation";
+// const sidebar = {
+//   open: (height = 1000) => ({
+//     clipPath: `circle(${height * 2 + 200}px at 100% 0)`,
+//     transition: {
+//       type: "spring",
+//       stiffness: 20,
+//       restDelta: 2,
+//     },
+//   }),
+//   closed: {
+//     clipPath: "circle(0px at 100% 0)",
+//     transition: {
+//       type: "spring",
+//       stiffness: 400,
+//       damping: 40,
+//     },
+//   },
+// };
+const menuVariants = {
+  open: {opacity: 1, x: 0},
+  closed: {opacity: 0, x: "-100%"},
 };
 
 const navItems = ["pricing", "changelog"];
 
 export default function MobileNav() {
-  const [isOpen, toggleOpen] = useCycle(false, true);
+  const [isOpen, setIsOpen] = useState(false);
+
   const containerRef = useRef(null);
-  const {height} = useDimensions(containerRef);
+  // const {height} = useDimensions(containerRef);
   const {showCartPreview, setShowCartPreview, cartTotalQuantity} = useCart();
 
   const toggleCart = () => {
     setShowCartPreview(!showCartPreview);
   };
 
-  return (
-    <motion.nav
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-      custom={height}
-      className={` top-0 inset-0  z-[50] lg:hidden w-full`}
-      ref={containerRef}
-    >
-      <motion.div
-        className="absolute inset-0 left-0 w-full bg-white"
-        variants={sidebar}
-      />
-      <motion.ul
-        variants={variants}
-        className="absolute grid w-full gap-3 px-10 py-16 "
-      >
-        {marketingConfig.mainNav.map((item, i) => (
-          <div key={i} className="grid gap-3">
-            <MenuItem>
-              <Link
-                href={item.href}
-                onClick={() => toggleOpen()}
-                className="flex w-full font-semibold capitalize"
-              >
-                {item.title}
-              </Link>
-            </MenuItem>
-            <MenuItem className="my-3 h-px w-full bg-gray-300" />
-          </div>
-        ))}
-      </motion.ul>
+  const router = useRouter();
+  const clickLink = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isOpen]);
 
+  return (
+    <>
       <div className="flex items-center justify-between px-4 relative z-20  py-2 bg-white">
         <Link href="/#" className="">
           <span className="text-2xl text-primary font-bold  flex items-center ">
@@ -99,16 +92,35 @@ export default function MobileNav() {
             )}
             <Icons.shoppingBag className="h-6 w-6 " id="header-cart-icon" />
           </button>
-          <MenuToggle toggle={toggleOpen} isOpen={isOpen} />
+          <MenuToggle setIsOpen={setIsOpen} isOpen={isOpen} />
         </div>
       </div>
-    </motion.nav>
+      <nav
+        className={cn(
+          "fixed inset-0 top-12 z-20 hidden w-full bg-white px-5  lg:hidden",
+          isOpen && "block"
+        )}
+      >
+        <ul className="grid divide-y divide-gray-200">
+          {marketingConfig.mainNav.map((item, i) => (
+            <li key={i} className="grid gap-3">
+              <Button
+                onClick={() => clickLink(item.href)}
+                className="flex w-full bg-white text-theme-blue border-none font-semibold capitalize"
+              >
+                {item.title}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
 
-const MenuToggle = ({toggle, isOpen}: {toggle: any; isOpen: boolean}) => (
+const MenuToggle = ({setIsOpen, isOpen}: {setIsOpen: any; isOpen: boolean}) => (
   <button
-    onClick={toggle}
+    onClick={() => setIsOpen(!isOpen)}
     className="pointer-events-auto z-20 aspect-square p-2 text-theme-blue"
   >
     {isOpen ? (
@@ -170,14 +182,47 @@ const variants = {
   },
 };
 
-const useDimensions = (ref: any) => {
-  const dimensions = useRef({width: 0, height: 0});
+// const useDimensions = (ref: any) => {
+//   const dimensions = useRef({width: 0, height: 0});
 
-  useEffect(() => {
-    dimensions.current.width = ref.current.offsetWidth;
-    dimensions.current.height = ref.current.offsetHeight;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//   useEffect(() => {
+//     dimensions.current.width = ref.current.offsetWidth;
+//     dimensions.current.height = ref.current.offsetHeight;
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
-  return dimensions.current;
-};
+//   return dimensions.current;
+// };
+
+// <motion.nav
+//   initial={false}
+//   animate={isOpen ? "open" : "closed"}
+//   custom={height}
+//   className={` lg:hidden `}
+//   ref={containerRef}
+//   variants={menuVariants}
+// >
+//   <motion.div
+//     className="fixed inset-0 z-[50] left-0 w-full bg-white"
+//     variants={variants}
+//   />
+//   <motion.ul
+//     variants={variants}
+//     className="fixed grid w-full gap-3  px-10 py-16 b-b"
+//   >
+//     {marketingConfig.mainNav.map((item, i) => (
+//       <div key={i} className="grid gap-3">
+//         <MenuItem>
+//           <Link
+//             href={item.href}
+//             onClick={() => setIsOpen(!isOpen)}
+//             className="flex w-full font-semibold capitalize"
+//           >
+//             {item.title}
+//           </Link>
+//         </MenuItem>
+//         <MenuItem className="my-3 h-px w-full bg-gray-300" />
+//       </div>
+//     ))}
+//   </motion.ul>
+// {/* </motion.nav> */}
