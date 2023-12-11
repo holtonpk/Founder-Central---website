4845591f2f1f7a4a36be3a2e78479db6f733b350
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-12-11 14:14:44 -0700

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import "@/app/(client)/client-style.css";
 import {siteConfig} from "@/config/site";
 import {constructMetadata} from "@/lib/utils";
+import Script from "next/script";
 
 export const metadata = constructMetadata({
   title: siteConfig.name,
@@ -11,8 +12,8 @@ export const metadata = constructMetadata({
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <!-- Meta Pixel Code -->
-<script>
+      <Script>
+        {`
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -22,12 +23,17 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '350950157581468');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=350950157581468&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
+fbq('track', 'PageView')`}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          className="hidden"
+          src="https://www.facebook.com/tr?id=350950157581468&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       <body className="">
         <main>{children}</main>
       </body>
