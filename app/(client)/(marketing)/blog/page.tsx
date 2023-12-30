cc452b381306bb225d1ca353aecf5523cf1b829f
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-12-29 18:21:05 -0700

    blog

--- app/(client)/(marketing)/blog/page.tsx
+++ app/(client)/(marketing)/blog/page.tsx
@@ -10,7 +10,7 @@ const Page = async () => {
   });
 
   return (
-    <div className="container max-w-4xl py-6 lg:pb-10 pt-28">
+    <div className="container max-w-4xl py-6 lg:pb-10 md:pt-28">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl text-theme-blue font-head font-bold">
